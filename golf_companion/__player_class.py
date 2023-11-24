86623bbc2ecdfd3a82c61d974426988cc65e3ce9
Tim Pulfer <timothypulfer@shaw.ca>
2023-11-24 12:25:55 -0800

    adding player class

--- golf_companion/__player_class.py
+++ golf_companion/__player_class.py
@@ -1,41 +1,43 @@
 import pandas as pd
+import numpy as np
 
-class Course:
-    def __init__(self, course_name, file_path, course_record=None):
-        self.course_name = course_name
-        try:
-            score_card = pd.read_csv(file_path)
-        except:
-            raise ValueError("Error with file or file path, please try again.")
-        self.score_card = score_card
-        self.course_record = course_record
-        try:
-            par = sum(self.score_card['par'])
-        except:
-            raise ValueError("Could not read course par values, make sure they are integers")
-        self.par = int(par)
-        
-    def add_course(course_name, file_path, course_record=None):
-        try:
-            pd.read_csv(file_path)
-        except:
-            return "ERROR"
-        return Course(course_name, file_path, course_record)
-    
+class Player:
+    professional_yardages = {
+    "Driver": 280,
+    "3 Wood": 240,
+    "5 Wood": 225,
+    "4 Iron": 210,
+    "5 Iron": 195,
+    "6 Iron": 180,
+    "7 Iron": 165,
+    "8 Iron": 150,
+    "9 Iron": 135,
+    "Pitching Wedge": 120,
+    "Sand Wedge": 110,
+    "Putter": 30
+    }
+
+    def __init__(self, name, skill, handicap=None,score=None):
+        self.name = name
+        self.handicap = handicap
+        self.skill = skill
+        self.score= score
+        self.yardages = self.calculate_yardages(skill)
+
+    #Skill level 1 = pro
+    def calculate_yardages (self,skill):
+        beginner_multiplier = 0.6  # 60% of professional yardage
+        intermediate_multiplier = 0.8  # 80% of professional yardage
+        if skill == 1:
+            return self.professional_yardages
+        elif skill == 2:
+            return {club: int(yardage * intermediate_multiplier) for club, yardage in professional_yardages.items()}
+        elif skill == 3: 
+            return {club: int(yardage * beginner_multiplier) for club, yardage in professional_yardages.items()}
+
     def __str__(self):
-        pd.set_option('display.max_rows', None)
-        pd.set_option('display.max_columns', None)
-        return f"\nCourse: {self.course_name} \nCourse Record: {self.course_record}\nPar: {self.par} \nScore Card:\n{self.score_card.T}\n"
-
-
-sunset_ranch_course_record = 65
-sunset_ranch_course = Course("Sunset Ranch","sunset_ranch.csv", sunset_ranch_course_record)
-
-shadow_ridge_course_record = 64
-shadow_ridge_course = Course("Shadow Ridge", "shadow_ridge.csv", shadow_ridge_course_record)
-
-okanagan_golf_club_bear_course_record = 68
-okanagan_golf_club_bear_course = Course("Okanagan Golf Club Bear","okanagan_golf_club_bear.csv", okanagan_golf_club_bear_course_record)
-
-pd.reset_option('display.max_rows')
-pd.reset_option('display.max_columns')
+        skill_level = "Professional" if self.skill == 1 else "Intermediate" if self.skill == 2 else "Amateur"
+        output = f"Player: {self.name}\nSkill Level: {skill_level}\nHandicap: {self.handicap if self.handicap is not None else 'N/A'}\n"
+        if self.score is not None:
+            output += "Score:\n" + str(pd.DataFrame([self.score], index=[self.name]))
+        return output
