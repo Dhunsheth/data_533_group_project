9ff2c6f96c7cbc2d4b75afe89d5027c382ec7f18
Tim Pulfer <timothypulfer@shaw.ca>
2023-11-24 13:56:47 -0800

    Added start_picking

--- golf_companion/pick_club/start_picking.py
+++ golf_companion/pick_club/start_picking.py
@@ -1,7 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 23 20:39:59 2023
+import __player_class 
 
-@author: Dhuns
-"""
+def start_picking(target_yards, player):
+    if not isinstance(target_yards, (int, float)) or target_yards < 0:
+        raise ValueError("Target yards must be a positive number.")
 
+    best_club = None
+    min_diff = float('inf')
+
+    for club, yardage in player.yardages.items():
+        diff = target_yards - yardage
+        if 0 <= diff < min_diff:
+            best_club = club
+            min_diff = diff
+
+    if best_club is None:
+        return "No suitable club found."
+
+    return best_club
+
+p = __player_class .Player("Tim",2)
+best_club= start_picking(200,p)
+print(best_club)
